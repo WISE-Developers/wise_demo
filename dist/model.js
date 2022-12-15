5c8675e0d78ee8a452af1a9fe2e054b63c75258b
Franco Nogarin <spydmobile@gmail.com>
2022-12-15 09:47:10 -0700

    keep the container running

--- dist/model.js
+++ dist/model.js
@@ -1,18 +1,5 @@
 "use strict";
 // iife to start the service
-(async function () {
-    console.clear();
-    // create a new instance of the service
-    console.log("Creating a new instance of the service");
-    const idle = () => {
-        console.log("Idle");
-        const test = true;
-        return new Promise((resolve) => {
-            if (!test) {
-                resolve('test');
-            }
-        });
-    };
-    await idle();
-    console.log("Done");
+(function keepProcessRunning() {
+    setTimeout(keepProcessRunning, 1 << 30);
 })();
