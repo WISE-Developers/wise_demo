5d79d51a9e57ab4ffce0cb9b3f1f7bd9237bf69d
Franco Nogarin <spydmobile@gmail.com>
2023-01-22 08:49:55 -0700

    fix job logic

--- dist/model.js
+++ dist/model.js
@@ -36,38 +36,39 @@ catch (error) {
     console.log('Error clearing old results');
     console.log(error);
 }
-console.log('Running the job');
-(0, child_process_1.exec)("/usr/bin/psaas ~/app_data/testjob/job.fgmj --validate", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log("The job validated");
-    console.log(`stdout: ${stdout}`);
-});
-(0, child_process_1.exec)("/usr/bin/psaas ~/app_data/testjob/job.fgmj", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log("The job was executed");
-    console.log(`stdout: ${stdout}`);
-    // read in the file ~/app_data/testjob/Outputs/bestSummary.txt
-    // and return the contents
-    console.log('Reading the results');
-    const bestSummary = fs.readFileSync('/root/app_data/testjob/Outputs/bestSummary.txt', 'utf8');
-    console.log(bestSummary);
-    const worstSummary = fs.readFileSync('/root/app_data/testjob/Outputs/worstSummary.txt', 'utf8');
-    console.log(worstSummary);
-});
+async function modelLogic() {
+    console.log('Running the job');
+    await (0, child_process_1.exec)("/usr/bin/wise ~/app_data/testjob/job.fgmj --validate", (error, stdout, stderr) => {
+        if (error || stderr) {
+            console.log(`error: ${error?.message}`);
+            console.log(`stderr: ${stderr}`);
+            process.exit(1);
+            return;
+        }
+        console.log("The job validated");
+        console.log(`stdout: ${stdout}`);
+    });
+    (0, child_process_1.exec)("/usr/bin/wise ~/app_data/testjob/job.fgmj", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log("The job was executed");
+        console.log(`stdout: ${stdout}`);
+        // read in the file ~/app_data/testjob/Outputs/bestSummary.txt
+        // and return the contents
+        console.log('Reading the results');
+        const bestSummary = fs.readFileSync('/root/app_data/testjob/Outputs/bestSummary.txt', 'utf8');
+        console.log(bestSummary);
+        const worstSummary = fs.readFileSync('/root/app_data/testjob/Outputs/worstSummary.txt', 'utf8');
+        console.log(worstSummary);
+    });
+}
+modelLogic();
 // console.log("starting the keep alive process");
 // // iife to start the keepalive process
 // (function keepProcessRunning() {
